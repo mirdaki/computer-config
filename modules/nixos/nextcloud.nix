884fc65f1f8d93372ea4cfb5835be96950033358
mirdaki <mirdaki@users.noreply.github.com>
2025-05-24 11:36:13 -0700

    Update to nixos 25.05

--- modules/nixos/nextcloud.nix
+++ modules/nixos/nextcloud.nix
@@ -51,7 +51,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud30;
+      package = pkgs.nextcloud31;
       hostName = "${cfg.subDomainName}.${cfg.baseDomainName}";
       https = true;
       datadir = cfg.dataDir;
