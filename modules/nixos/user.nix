80cf455990b079a2b02056c473318d6b98463825
mirdaki <mirdaki@users.noreply.github.com>
2026-03-21 20:17:57 -0700

    Update user module

--- modules/nixos/user.nix
+++ modules/nixos/user.nix
@@ -14,26 +14,49 @@ in
 
     user.name = lib.mkOption { type = lib.types.str; };
 
-    user.hashedPasswordFile = lib.mkOption { type = lib.types.str; };
+    user.shell = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.bash;
+    };
+
+    user.hashedPasswordFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+    };
+
+    user.enableSshKeys = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    user.extraGroups = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
   };
 
   config = lib.mkIf cfg.enable {
     users = {
-      # This prevents using normal bash commands to create/modify users
-      mutableUsers = false;
+      # False prevents using normal bash commands to create/modify users
+      mutableUsers = (cfg.hashedPasswordFile == null);
       users.${cfg.name} = {
         isNormalUser = true;
-        hashedPasswordFile = cfg.hashedPasswordFile;
-        description = "main user";
+        description = cfg.name;
         extraGroups = [
           "networkmanager"
           "wheel"
-        ];
-        shell = pkgs.bash;
-        openssh.authorizedKeys.keyFiles = [
+        ]
+        ++ cfg.extraGroups;
+        # Shell needs to be set here to automatically start, even though it's configured in home-manager
+        shell = cfg.shell;
+        openssh.authorizedKeys.keyFiles = lib.mkIf cfg.enableSshKeys [
           ../../modules/nixos/ssh-keys/mandalore/id_rsa.pub
           ../../modules/nixos/ssh-keys/corellia/id_rsa.pub
         ];
+      }
+      # This merges attribute sets
+      // lib.optionalAttrs (cfg.hashedPasswordFile != null) {
+        hashedPasswordFile = cfg.hashedPasswordFile;
       };
     };
   };
