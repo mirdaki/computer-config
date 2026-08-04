86be5ec07d825862f4d597076bc4bce52bca36e6
mirdaki <mirdaki@users.noreply.github.com>
2026-08-03 21:01:14 -0700

    Update bespin to 26.05

--- modules/nixos/nextcloud.nix
+++ modules/nixos/nextcloud.nix
@@ -53,7 +53,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud32;
+      package = pkgs.nextcloud33;
       hostName = "${cfg.subDomainName}.${cfg.baseDomainName}";
       https = true;
       datadir = cfg.dataDir;
